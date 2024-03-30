e9aed393d58865bdfe2ec6a0100a2154732fde46
hyun <happy951219@naver.com>
2024-03-30 09:23:53 +0900

    타입스크립트 설정 (#1)

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,23 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
-  devtool: 'source-map',
+  mode: 'production',
+  devtool: 'hidden-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css.ts$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      linkType: false,
+      filename: '[name]..css',
+      chunkFilename: '[name].css',
+    }),
+  ],
 });
