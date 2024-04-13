8a6cc5bf543253f5974e910aaa18a06a362dd94a
hyun <happy951219@naver.com>
2024-04-13 09:27:29 +0900

    feat: add global style tokens

---
+++ src/shared/consts/theme/fonts.ts
@@ -0,0 +1,8 @@
+export const fontSizes = {
+  '10px': '10px',
+  '12px': '12px',
+  '14px': '14px',
+  '16px': '16px',
+  '18px': '18px',
+  '20px': '20px',
+};
---
+++ src/shared/consts/theme/global.css.ts
@@ -0,0 +1,6 @@
+import { sprinkles } from '@/shared/consts/theme/sprinkles.css';
+
+export const flexCenter = sprinkles({
+  display: 'flex',
+  alignItems: 'center',
+});
